c26d59d4d004eb1b9adefc50d549ce035f6342d6
tusharupadhyay_mobiloitte <do-tushar@mobiloitte.com>
2018-12-25 10:15:05 +0530

    Logo design, Login Page Logo

--- Nexamind/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
+++ Nexamind/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
@@ -35,13 +35,13 @@ $(document).ready(function () {
         }
     });
 
-    $(".nexamind-signout").click(function () {
+    function signOut() {
             var auth2 = gapi.auth2.getAuthInstance();
             auth2.signOut().then(function () {
                 console.log('User signed out.');
             });
         
-    });
+    };
 
     //$(".nexamind-onselect-bg-color-change").hover(function () {
 
