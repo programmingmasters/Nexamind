6e66fecb1a29b6215fd45a0aecaae129337cfddb
do-tushar@mobiloitte.com <do-tushar@mobiloitte.com>
2018-12-29 18:00:24 +0530

    Profile Info and Edit

---
+++ Nexamind.ViewModel/ApplicationViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nexamind.ViewModel.ApplicationViewModel
+{
+    public class HomeViewModel : BaseModel
+    {
+
+    }
+}
--- Nexamind/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
+++ Nexamind/obj/Release/netcoreapp2.2/PubTmp/Out/wwwroot/js/site.js
@@ -55,4 +55,5 @@ $(document).ready(function () {
     //        $(this).css("background-color", "transparent");
     //    }
     //});
+
 });
