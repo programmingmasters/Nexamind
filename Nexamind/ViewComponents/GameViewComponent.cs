42de6fc9167c3003fa9af3ac661f9b767d3012c1
Tushar Upadhyay <do-tushar@mobiloitte.com>
2019-03-16 17:15:37 +0530

    ViewComponent experiment

---
+++ Nexamind/ViewComponents/GameViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexamind.BO.UserRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nexamind.ViewComponents
+{
+    public class GameViewComponent : ViewComponent
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GameViewComponent(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userRepository.GetAllUsers();
+            return View(user);
+        }
+    }
+}
